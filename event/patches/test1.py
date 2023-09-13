45c76bf24ac44d01b078af41a9e4d45776de2004
Bhavatosh-Sigzen <bhavatosh@sigzen.com>
2023-09-13 14:35:40 +0530

    changes in patches

---
+++ event/patches/test1.py
@@ -0,0 +1,25 @@
+# from frappe import _
+# import frappe
+# from frappe.utils import getdate, nowdate
+
+# def execute():
+#     custom_type_docs = frappe.get_all("Custom Type", filters={"date_of_birth"}, fields=["name", "date_of_birth"])
+
+#     for doc in custom_type_docs:
+        
+#         date_of_birth = doc.date_of_birth
+
+        
+#         if date_of_birth:
+           
+#             dob = getdate(date_of_birth)
+
+         
+#             today = nowdate()
+
+#             age = today.year - dob.year - ((today.month, today.day) < (dob.month, dob.day))
+
+#             frappe.db.set_value("Custom Type", doc.name, "age", age)
+
+#     frappe.msgprint(("Age calculation completed for Custom Type documents."))
+
